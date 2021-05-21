4e7c6a817cff881eea983d5a2293a81dd27bf4d9
skyco2020 <skyco2020streaming@gmail.com>
2021-05-21 02:28:18 -0300

    pantalla principal no terminada

--- TPFinal/Front-End/Kayak/src/app/Route/route.ts
+++ TPFinal/Front-End/Kayak/src/app/Route/route.ts
@@ -1,7 +1,13 @@
 import { Routes } from "@angular/router";
 import { AccountComponent } from "../components/account/account/account.component";
+import { PrincipalComponent } from '../components/principal/principal/principal.component';
 
 export const appRoutes: Routes = [
+  {
+    path: 'Home',
+    component: PrincipalComponent,
+     data: {title: 'Bienvenido al sitio de Kayak'}
+  },
   {
     path: 'Account',
     component: AccountComponent,
@@ -9,7 +15,7 @@ export const appRoutes: Routes = [
   },
   {
     path: '',
-    redirectTo: `/Account`,
+    redirectTo: `/Home`,
     pathMatch: 'full',
   }
 ];
